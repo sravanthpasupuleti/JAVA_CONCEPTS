c9bc4efd6286be8d74626b187496348c0694e6aa
sravanth pasupuleti <sravanthpasupuleti@gmail.com>
2026-01-17 22:28:29 +0530

    practiced Streamssss

---
+++ JAVA-8/streamproblems/findEvenAsc.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class findEvenAsc {
+    public static void main(String[] args) {
+        Integer[] numbers = {3,6,1,8,5,10,7};
+
+        // List<Integer> numbers = new ArrayList<>(Arrays.asList(3,6,1,8,5,10,7));
+
+        //find even , calculate their squares and print in increasing order and also show how many
+          //numbers met the criteria.
+          
+        long c = Arrays.stream(numbers)
+                .filter(num -> num%2 == 0)
+                .map(num -> num*num).sorted()
+                .peek(System.out::println).count();
+
+        System.out.println(c);
+    }
+
+}
---
+++ JAVA-8/streamproblems/findListEmps.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class findListEmps{
+    public static void main(String[] args) {
+    List<String> emps = new ArrayList<>(Arrays.asList("John", "Alexandra" , "Ben" , "David" , "Sravanth" , "Amy"));
+
+    
+
+    //create a stream to find the the emps whose name atleast min 5 char and convert theor names into
+        //uppercase and finally print those names in alphabitically order.
+
+    emps.stream().filter(name -> name.length()>=5).map(String::toUpperCase).forEach(System.out::println);
+
+    }
+}
