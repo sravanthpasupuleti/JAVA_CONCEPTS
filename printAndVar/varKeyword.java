5306746db8c824daff2aa7f6ee65c5e0bc73f731
sravanth pasupuleti <sravanthpasupuleti@gmail.com>
2026-01-22 12:46:22 +0530

    var Keyword

---
+++ printAndVar/varKeyword.java
@@ -0,0 +1,9 @@
+public class varKeyword{
+    public static void main(String[] args){
+        var i =2;
+        var s = "hiii";
+
+        System.out.println(i);
+        System.out.println(s);
+    }
+}
